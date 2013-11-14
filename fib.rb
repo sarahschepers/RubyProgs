ec3cd7f7b9ae18a944f4ddd9773177d2394e8b0d
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 13:28:10 +0100

    fibonacci changed

--- fib.rb
+++ fib.rb
@@ -1,16 +1,9 @@
 def main
-  printf "\nHow many numbers of the sequence would you like?\n"
-
-  # Here, STDIN is a constant, but like everything in Ruby, it's also a class, so
-  # we use the readline method to get our input. to_i casts our input to an integer
+  printf "\nHoeveel getallen van de Rij van Fibonacci wilt u tonen?\n"
 
   n = STDIN.readline.to_i
   fibonacci(n)
 end
-
-# Here is a good example of something that is cool about Ruby. The times method
-# works just like it sounds, it will do something n times. As in Perl and Python,
-# we don't need a temp variable here to swap the values for a and b.
 
 def fibonacci(n)
   a,b = 0,1
