93cdd35a9992a2bb240deb9e40c8c89edf1e3dc7
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 13:38:18 +0100

    reverse commit

---
+++ reverse.rb
@@ -0,0 +1,4 @@
+puts "Dit programma telt het aantal letters en woorden in een zin en geeft de zin omgekeerd weer. Geef een zin in:"
+zin = gets.to_f
+aantalLetters = zin.length
+puts "Het aantal letters in deze zin is: #{aantalLetters}"
