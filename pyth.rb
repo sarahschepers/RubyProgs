9c9a4a589f597bb3622a3338f15638a629d414c2
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 13:11:11 +0100

    pyth changed

--- pyth.rb
+++ pyth.rb
@@ -1,8 +1,8 @@
-puts "Welcome to my pythagorean theorem calculator. This calculator can find the hypotenuse of any right triangle. Please enter side A of your triangle."
+puts "Dit is een programma om de stelling van Pythagoras toe te passen op een rechthoekige driehoek. Geef de lengte van zijde A in:"
 a = gets.to_f
-puts "please enter side B of your triangle."
+puts "Geef de lengte van zijde B in:"
 b = gets.to_f
 a2 = a**2
 b2 = b**2
 a2_b2 = a2 + b2 
-puts "The hypotenuse of your triangle is: #{ Math.sqrt(a2_b2)}"
+puts "De schuine zijde is: #{ Math.sqrt(a2_b2)}"
