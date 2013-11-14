9019e9e272250fb102f2e95211558de315d6f514
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 14:10:13 +0100

    rubyprog2 aangepast

--- RubyProg2.rb
+++ RubyProg2.rb
@@ -2,7 +2,7 @@ def fact(n)
 	if n == 0
 		1
 	else
-		n * fact(n-­-1)
+		n * fact(n-1)
 	end
 end
 
--- reverse.rb
+++
@@ -1,5 +0,0 @@
-puts "Dit programma telt het aantal letters en woorden in een zin en geeft de zin omgekeerd weer. De zin die wordt gebruikt is 'Het is herfst'."
-zinArray = []
-zin = gets.to_f
-zinArray.push(zin)
-puts zinArray.inspect
