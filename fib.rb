8b4cc46c41d9f36b5e882f152c17882baddd48a9
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 12:57:23 +0100

    fib changed 2

--- fib.rb
+++ fib.rb
@@ -2,4 +2,13 @@ def fibonacci( n )
     return  n  if n <= 1 
     fibonacci( n - 1 ) + fibonacci( n - 2 )
 end 
+puts fibonacci( 1 )
+puts fibonacci( 2 )
+puts fibonacci( 3 )
+puts fibonacci( 4 )
+puts fibonacci( 5 )
+puts fibonacci( 6 )
+puts fibonacci( 7 )
+puts fibonacci( 8 )
+puts fibonacci( 9 )
 puts fibonacci( 10 )
