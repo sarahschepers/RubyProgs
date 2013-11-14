b819436fa9c5e222a740404efe0084de75127a0c
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 12:54:19 +0100

    fib changed

--- fib.rb
+++ fib.rb
@@ -1,21 +1,5 @@
-def fibonacci(n)
-	result = ""
-	if n >= 1:
-		a = 1
-		b = 1
-		c = a + b
-		fibs = [b]
-		while n >= c:
-			fibs.append(c)
-			a = b
-			b = c
-			c = a + b
-		result = "1"
-		for fibnum in reversed(fibs):
-			if n >= fibnum:
-				n = n - fibnum
-				result = "1" + result
-			else:
-				result = "0" + result
-		return result
-print fibonacci(65)
+def fibonacci( n )
+    return  n  if n <= 1 
+    fibonacci( n - 1 ) + fibonacci( n - 2 )
+end 
+puts fibonacci( 10 )
