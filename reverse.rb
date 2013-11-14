0211de91974f17c1aad11b73f67ba9a67b4a0f0c
Sarah Schepers <1033142@student.khlim.be>
2013-11-14 14:05:37 +0100

    reverse aangepast

--- reverse.rb
+++ reverse.rb
@@ -1,3 +1,5 @@
 puts "Dit programma telt het aantal letters en woorden in een zin en geeft de zin omgekeerd weer. De zin die wordt gebruikt is 'Het is herfst'."
-zin = ["Het", "is", "herfst"]
-puts "Aantal woorden: #{zin.length}"
+zinArray = []
+zin = gets.to_f
+zinArray.push(zin)
+puts zinArray.inspect
